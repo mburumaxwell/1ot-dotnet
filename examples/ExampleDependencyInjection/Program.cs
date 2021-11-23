b4426eae92d73d28a559366379c5a1e3af0510c9
Maxwell Weru <mburumaxwell@gmail.com>
2021-11-23 13:22:54 +0300

    Use top level statements in tests

--- examples/ExampleDependencyInjection/Program.cs
+++ examples/ExampleDependencyInjection/Program.cs
@@ -1,40 +1,30 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Mobi1ot;
 using System;
-using System.Threading.Tasks;
 
-namespace ExampleDependencyInjection
+Console.WriteLine("Hello World!");
+
+var username = "your-username-here";
+var password = "your-password-here";
+
+var services = new ServiceCollection();
+services.AddMobi1ot(username: username, password: password);
+
+var provider = services.BuildServiceProvider();
+
+var client = provider.GetRequiredService<Mobi1otClient>();
+
+var response = await client.GetAccountSimsAsync();
+var resource = response.Resource;
+Console.WriteLine($"Found {resource.Found} of {resource.Total} from {resource.Offset} offset");
+Console.WriteLine("=====================");
+var simcards = resource.Sims;
+foreach (var sim in simcards)
 {
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-
-            var username = "your-username-here";
-            var password = "your-password-here";
-
-            var services = new ServiceCollection();
-            services.AddMobi1ot(username: username, password: password);
-
-            var provider = services.BuildServiceProvider();
-
-            var client = provider.GetRequiredService<Mobi1otClient>();
-
-            var response = await client.GetAccountSimsAsync();
-            var resource = response.Resource;
-            Console.WriteLine($"Found {resource.Found} of {resource.Total} from {resource.Offset} offset");
-            Console.WriteLine("=====================");
-            var simcards = resource.Sims;
-            foreach (var sim in simcards)
-            {
-                Console.WriteLine($"MSISDN: {sim.MSISDN}");
-                Console.WriteLine($"Name: {sim.Name}");
-                Console.WriteLine($"IMEI: {sim.IMEI}");
-                Console.WriteLine($"IMSI: {sim.IMSI}");
-                Console.WriteLine($"Status: {sim.Status.GetDescription()}");
-                Console.WriteLine("=====================");
-            }
-        }
-    }
+    Console.WriteLine($"MSISDN: {sim.MSISDN}");
+    Console.WriteLine($"Name: {sim.Name}");
+    Console.WriteLine($"IMEI: {sim.IMEI}");
+    Console.WriteLine($"IMSI: {sim.IMSI}");
+    Console.WriteLine($"Status: {sim.Status.GetDescription()}");
+    Console.WriteLine("=====================");
 }
